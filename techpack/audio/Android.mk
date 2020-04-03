fd2dd6513176efb9a5279e56371014d108f91b25
SamarV-121 <samarvispute121@gmail.com>
2020-04-03 13:23:23 +0530

    techpack: audio: Import Realme changes
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- techpack/audio/Android.mk
+++ techpack/audio/Android.mk
@@ -35,3 +35,23 @@ $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codec
 include $(MY_LOCAL_PATH)/asoc/codecs/sdm660_cdc/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/msm_sdw/Android.mk
 endif
+
+#ifdef VENDOR_EDIT
+#Kaiqin.Huang@RM.MM.AudioDriver.Codec, 2019/10/12, Add for tfa9890 codec
+include $(MY_LOCAL_PATH)/asoc/codecs/tfa98xx/Android.mk
+#endif /* VENDOR_EDIT */
+
+#ifdef VENDOR_EDIT
+#Jianfeng.Qiu@PSW.MM.AudioDriver.Codec, 2018/04/20, Add for tfa9894 codec
+include $(MY_LOCAL_PATH)/asoc/codecs/tfa98xx-v6/Android.mk
+#endif /* VENDOR_EDIT */
+
+#ifdef VENDOR_EDIT
+#Kaiqin.Huang@PSW.MM.AudioDriver.Codec, 2019/09/27, Remove ak43xx codec
+#include $(MY_LOCAL_PATH)/asoc/codecs/ak4376/Android.mk
+#endif /* VENDOR_EDIT */
+
+#ifdef VENDOR_EDIT
+#Kaiqin.Huang@PSW.MM.AudioDriver.Codec, 2019/09/27, Remove dbmdx
+#include $(MY_LOCAL_PATH)/asoc/codecs/dbmdx/Android.mk
+#endif /* VENDOR_EDIT */
